c7096126b238930698f8899712c8a2c373db2687
Blake Bourque <techplex.engineer@gmail.com>
2023-06-12 20:34:47 -0400

    fix generation paths

--- get-spec-and-build.sh
+++ get-spec-and-build.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 curl -o openapi.json https://cad.onshape.com/api/openapi
 
-rm -rf src/generated-sources/openapi
+rm -rf src/lib/onshape_api
 
 npx @openapitools/openapi-generator-cli generate \
 	-i ./openapi.json \
-	-o src/onshape_api \
+	-o src/lib/onshape_api \
 	-g typescript-fetch \
 	--additional-properties=supportsES6=true,npmVersion=6.9.0,typescriptThreePlus=true
